b908996a7b6d7d867bbcc145a3a6b37df6fe1407
IgnatievMisha <mishaross1604@gmail.com>
2023-05-04 19:20:55 +0300

    count examples(skolko raz vizivalosb)

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 #classes
 class Student():
     print("Hi!")
+    count=0
     def __init__(self, height=150):
         self.height = height
+        Student.count+=1
 oleg = Student() #object, ekzemplyar klacu
 print(oleg.height)
 masha = Student(height=200)
 print(masha.height)
+print(Student.count)
